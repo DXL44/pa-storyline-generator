a8c152b1084cef3f2deaffe1139d0ae228e2bc11
DXL44 <28364459+DXL44@users.noreply.github.com>
2023-12-04 11:21:11 -0500

    no node + this is now functional

--- main.js
+++ main.js
@@ -1,7 +1,52 @@
-const { request } = require('undici');
-
-async function getCoolName(letter1,letter2) {
-    const randomAdjective = await request(`https://random-word-form.repl.co/random/adejctive`);
-    const randomNoun = await request(`https://random-word-form.repl.co/random/noun`);
-    console.log(randomAdjective, randomNoun)
+function getWord(wordType)
+{
+    axios.get(`https://random-word-form.repl.co/random/${wordType}`)
+    .then(response =>
+        {
+            console.log(response.data);
+            document.getElementById(`${wordType}Word`).innerHTML = response.data;
+        })
+        .catch(error =>console.error(error))
 }
+
+function getCoolName()
+{ // this wasnt working because i was misspelling adjective.......
+    getWord("adjective") 
+    getWord("noun")
+}
+/*async function getCoolName()
+{
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json()
+    return data;
+}*/
+
+
+/* function getCoolName()
+{
+let request = new XMLHttpRequest();
+request.open("GET", "https://random-word-form.repl.co/random/adejctive")
+request.send();
+request.onload = () =>{
+    console.log(request);
+    console.log(request.response);
+    if (request.status == 200) {
+        console.log(JSON.parse(request.response))
+    }  else {
+        console.log(`error ${request.status} ${request.statusText}`)
+    }
+}
+}  */ 
+
+/*
+async function getCoolName(letter1,letter2) {
+    const randomAdjective = await Request(`https://random-word-form.repl.co/random/adejctive`);
+    const randomNoun = await Request(`https://random-word-form.repl.co/random/noun`);
+    console.log(randomAdjective, randomNoun)
+}*/
+// CURRENT: https://builtin.com/software-engineering-perspectives/javascript-api-call
+
+
+// https://stackoverflow.com/questions/54445033/how-to-use-node-modules-on-a-traditional-website
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
+// https://random-word-form.repl.co/
